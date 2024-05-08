5d1518ef6230f7a6687835b3b61c0960eb68e69b
Martin <901824+martinstark@users.noreply.github.com>
2024-05-08 11:07:35 +0200

    feat: add explicit scheme_id_uri types

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,11 @@ export interface EMSG {
   presentation_time_delta?: number;
   presentation_time?: number;
   event_duration: number;
-  scheme_id_uri: string;
+  scheme_id_uri:
+    | "urn:scte:scte35:2013:xml"
+    | "https://aomedia.org/emsg/ID3"
+    | "urn:yospace:a:id3:2016"
+    | string;
   size: number;
   timescale: number;
   type: string;
